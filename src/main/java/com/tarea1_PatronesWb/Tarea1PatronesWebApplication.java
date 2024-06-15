cf77cc92ff6837001340b814a70309ba0a691662
JorgeAlfaro29 <jorgealfarog29@hotmail.com>
2024-06-14 20:38:28 -0600

    Initial commit

---
+++ src/main/java/com/tarea1_PatronesWb/Tarea1PatronesWebApplication.java
@@ -0,0 +1,13 @@
+package com.tarea1_PatronesWb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tarea1PatronesWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tarea1PatronesWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea1_PatronesWb/Tarea1PatronesWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea1_PatronesWb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tarea1PatronesWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
